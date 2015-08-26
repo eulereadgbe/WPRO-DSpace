664a648c6944278c697a94d84f84b3affef07fae
Elvi Nemiz <esnemiz@gmail.com>
2015-08-26 10:40:54 +0800

    Move community/collection logo to the right (cherry picked from commit b99e6d0)

--- dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/wpro.js
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/wpro.js
@@ -4,5 +4,8 @@ $(function() {
     navmenu.each(function(){
         if($(this).attr("href") == pgurl)
             $(this).parent().addClass("active");
-    })
+    });
+    var logo = $(".ds-logo-wrapper");
+    logo.addClass("pull-right");
+    logo.detach().prependTo("#aspect_artifactbrowser_CommunityViewer_div_community-search-browse,#aspect_artifactbrowser_CollectionViewer_div_collection-search-browse");
 });
