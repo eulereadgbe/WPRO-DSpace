3b80c76c2de8668e27091a828112cbd88eb0ca23
Elvi Nemiz <esnemiz@gmail.com>
2015-08-28 03:33:45 +0800

    Sortable columns in submission table (cherry picked from commit af1afec)

--- dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/wpro.js
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/wpro.js
@@ -9,3 +9,14 @@ $(function() {
     logo.addClass("pull-right");
     logo.detach().prependTo("#aspect_artifactbrowser_CommunityViewer_div_community-search-browse,#aspect_artifactbrowser_CollectionViewer_div_collection-search-browse");
 });
+
+$(document).ready(function()
+    {
+        var submissionTable = $("#aspect_submission_Submissions_table_completed-submissions");
+        submissionTable.addClass("tablesorter");
+        var theader = $(".ds-table-header-row").wrap("<thead></thead>");
+        var $toolbar = $('#aspect_submission_Submissions_table_completed-submissions > tbody > thead');
+        $toolbar.parent().before($toolbar);
+        submissionTable.tablesorter();
+    }
+);
