cf0c86955c71a22ccebe60f884c15c5aef37cdca
Elvi Nemiz <esnemiz@gmail.com>
2015-09-09 22:46:03 +0800

    Resort treeview alphabetically (cherry picked from commit e577287)

--- dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/wpro.js
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/wpro.js
@@ -17,7 +17,10 @@ $(function () {
             $('#expand-by-default').click();
         }
     });
-
+    $('ul > li > div > h4 > a').each(function (i) {
+            $(this).parentsUntil(".collapsable", "ul").attr('id', 'sort');
+    });
+    sortUnorderedList("sort");
 });
 
 $(document).ready(function () {
@@ -29,3 +32,33 @@ $(document).ready(function () {
         submissionTable.tablesorter();
     }
 );
+
+function sortUnorderedList(ul, sortDescending) {
+    if(typeof ul == "string")
+        ul = document.getElementById(ul);
+
+    // Idiot-proof, remove if you want
+    if(!ul) {
+        alert("The UL object is null!");
+        return;
+    }
+
+    // Get the list items and setup an array for sorting
+    var lis = ul.getElementsByTagName("a");
+    var vals = [];
+
+    // Populate the array
+    for(var i = 0, l = lis.length; i < l; i++)
+        vals.push(lis[i].innerHTML);
+
+    // Sort it
+    vals.sort();
+
+    // Sometimes you gotta DESC
+    if(sortDescending)
+        vals.reverse();
+
+    // Change the list on the page
+    for(var i = 0, l = lis.length; i < l; i++)
+        lis[i].innerHTML = vals[i];
+}
