7e54093702cbdac9e4ce2c83bd97aacab81f9b71
Elvi Nemiz <esnemiz@gmail.com>
2015-09-10 16:19:14 +0800

    Sort RCM collection titles in Descending order (cherry picked from commit 5190a38)

--- dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/wpro.js
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/wpro.js
@@ -17,10 +17,6 @@ $(function () {
             $('#expand-by-default').click();
         }
     });
-    $('ul > li > div > h4 > a').each(function (i) {
-            $(this).parentsUntil(".collapsable", "ul").attr('id', 'sort');
-        sortUnorderedList("sort");
-    });
 });
 
 $(document).ready(function () {
@@ -32,27 +28,3 @@ $(document).ready(function () {
         submissionTable.tablesorter();
     }
 );
-
-function sortUnorderedList(ul, sortDescending) {
-    if(typeof ul == "string")
-        ul = document.getElementById(ul);
-
-    // Get the list items and setup an array for sorting
-    var lis = ul.getElementsByTagName("a");
-    var vals = [];
-
-    // Populate the array
-    for(var i = 0, l = lis.length; i < l; i++)
-        vals.push(lis[i].innerHTML);
-
-    // Sort it
-    vals.sort();
-
-    // Sometimes you gotta DESC
-    if(sortDescending)
-        vals.reverse();
-
-    // Change the list on the page
-    for(var i = 0, l = lis.length; i < l; i++)
-        lis[i].innerHTML = vals[i];
-}
